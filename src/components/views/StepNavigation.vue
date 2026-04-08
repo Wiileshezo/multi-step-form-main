b29bb9faa6a372ad849e8f656169285b14729e88
Wiil Eshezo <wiileshezo@gmail.com>
2026-04-09 02:12:18 +0300

    add new components

---
+++ src/components/views/StepNavigation.vue
@@ -0,0 +1,12 @@
+<script setup>
+import StepPersonalInfo from "../steps/StepPersonalInfo.vue";
+</script>
+
+<template>
+  <div>
+    <h1>Step Navigation</h1>
+    <step-personal-info></step-personal-info>
+  </div>
+</template>
+
+<style scoped></style>
