79f369ddf85fffd56a5677e4f1a86bf184d895e1
Wiil Eshezo <wiileshezo@gmail.com>
2026-04-10 00:02:15 +0300

    add new component

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  type: String,
+  mode: String,
+});
+</script>
+
+<template>
+  <button :type="type" :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  border: none;
+  background: var(--Blue950);
+  color: var(--White);
+  cursor: pointer;
+  border-radius: 0.6rem;
+  padding: 0.8rem 1.5rem;
+  font-size: 1rem;
+  font-family: "Poppins", sans-serif;
+}
+
+@media screen and (min-width: 768px) {
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import App from "./App.vue";
 import MultiStepForm from "./components/views/MultiStepForm.vue";
 import TheFooter from "./components/layout/TheFooter.vue";
 import BaseCard from "./components/UI/BaseCard.vue";
+import BaseButton from "./components/UI/BaseButton.vue";
 
 const app = createApp(App);
 
 app.component("multi-step-form", MultiStepForm);
 app.component("the-footer", TheFooter);
 app.component("base-card", BaseCard);
+app.component("base-button", BaseButton);
 
 app.mount("#app");
