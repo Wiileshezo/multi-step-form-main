864852c70a25b82bc1e55081080af7b279314c25
Wiil Eshezo <wiileshezo@gmail.com>
2026-04-10 00:04:05 +0300

    write some content in the sidebar

--- src/components/steps/StepPersonalInfo.vue
+++ src/components/steps/StepPersonalInfo.vue
@@ -3,6 +3,7 @@
 <template>
   <div>
     <h1>Personal Info</h1>
+    <base-button mode="primary" type="submit">Next Step</base-button>
   </div>
 </template>
 
--- src/components/views/SidebarSteps.vue
+++ src/components/views/SidebarSteps.vue
@@ -1,8 +1,35 @@
 <script setup></script>
 
 <template>
-  <div class="sidebar-container">
-    <h1>Sidebar Steps</h1>
+  <div class="sidebar-container display-flex direction-column gap1">
+    <div class="display-flex direction-row gap1 align-items-center">
+      <h2 class="number">1</h2>
+      <div class="display-flex direction-column">
+        <p class="step">STEP 1</p>
+        <p class="step-title">YOUR INFO</p>
+      </div>
+    </div>
+    <div class="display-flex direction-row gap1 align-items-center">
+      <h2 class="number">2</h2>
+      <div class="display-flex direction-column">
+        <p class="step">STEP 2</p>
+        <p class="step-title">SELECT PLAN</p>
+      </div>
+    </div>
+    <div class="display-flex direction-row gap1 align-items-center">
+      <h2 class="number">3</h2>
+      <div class="display-flex direction-column">
+        <p class="step">STEP 3</p>
+        <p class="step-title">ADD-ONS</p>
+      </div>
+    </div>
+    <div class="display-flex direction-row gap1 align-items-center">
+      <h2 class="number">4</h2>
+      <div class="display-flex direction-column">
+        <p class="step">STEP 4</p>
+        <p class="step-title">SUMMARY</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +39,22 @@
   background-repeat: no-repeat;
   background-size: cover;
   width: 20vw;
+  box-sizing: border-box;
+  padding: 2.5rem;
+}
+
+.number {
+  color: var(--White);
+  padding: 0.5rem 1rem;
+  border: 1px solid var(--White);
+  border-radius: 50%;
+  font-size: 1rem;
+}
+.step {
+  color: var(--Grey500);
+}
+.step-title {
+  color: var(--White);
+  font-weight: 500;
 }
 </style>
