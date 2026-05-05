673f0e0fbdaf64c633bf7799433b5699d78be7ab
Wiil Eshezo <wiileshezo@gmail.com>
2026-05-05 16:24:22 +0300

    change the display

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
-<script setup></script>
+<script setup>
+import MultiStepForm from "./components/views/MultiStepForm.vue";
+import MobileNavbar from "./components/views/MobileNavbar.vue";
+</script>
 
 <template>
-  <multi-step-form></multi-step-form>
+  <mobile-navbar>
+    <MultiStepForm class="multi-step-form" />
+  </mobile-navbar>
+
+  <base-card class="btn-card">
+    <base-button class="next-btn" mode="primary" type="submit">
+      Next Step
+    </base-button>
+  </base-card>
+
   <the-footer />
 </template>
 
-<style></style>
+<style>
+.multi-step-form {
+  padding-inline: 2rem;
+}
+
+.btn-card {
+  display: none;
+  padding: 6rem 2rem 0 0;
+  z-index: 999;
+}
+</style>
