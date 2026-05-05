2407b8c11d7a6d48ad5269176c59e7e0732f3d82
Wiil Eshezo <wiileshezo@gmail.com>
2026-05-05 16:22:56 +0300

    add new component

---
+++ src/components/views/MobileNavbar.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+
+<template>
+  <div
+    class="sidebar-container-mobile display-flex direction-column justify-content-center gap1"
+  >
+    <div class="display-flex direction-row justify-content-center gap1">
+      <h2 class="number">1</h2>
+      <h2 class="number">2</h2>
+      <h2 class="number">3</h2>
+      <h2 class="number">4</h2>
+    </div>
+    <div class="display-flex direction-column">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.sidebar-container-mobile {
+  display: none;
+}
+@media (max-width: 770px) {
+  .sidebar-container-mobile {
+    background-image: url(@/images/bg-sidebar-mobile.svg);
+    display: flex;
+
+    padding-top: 3rem;
+
+    place-items: center;
+    z-index: 999;
+    width: 100vw;
+    min-height: 30vh;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    background-position: center;
+  }
+}
+</style>
